46b4547c2eccb7ddd7f95fb9aacb71fb34c84415
NothingCanStopMe <18521487@gm.uit.edu.vn>
2020-08-17 10:52:39 +0700

    Add module sayHi for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,14 @@
+var chalk = require('chalk');
 function Dog (){
+	this.name = 'Lucky';
 	this.stomatch = [];
 }
 
 Dog.prototype.eat = function(cat){
 	this.stomatch.push(cat); 
 }
+
+Dog.prototype.sayHi = function(){
+	console.log ('Hi, I am a dog and my name is ' + chalk.blue(this.name));
+};
+module.exports = Dog;
